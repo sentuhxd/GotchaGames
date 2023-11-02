8f4e834a3d5d91b2a884204b932e22f74c0087b5
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-11-02 14:31:28 -0400

    added image and working routing for ttt

--- client/src/components/Games/index.jsx
+++ client/src/components/Games/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
 import chuteImage from "../../assets/chutesboard.png";
+import tttImage from "../../assets/tictactoe.png";
 
 function Games() {
   function showGame() {
@@ -11,12 +12,22 @@ function Games() {
           <p className="mx-1">
             <Link to="/Chutes">Play Game</Link>
           </p>
+          <img src={tttImage}></img>
+          <p className="mx-1">
+            <Link to="/ttt">Play Game</Link>
+          </p>
         </>
+        
+        
       );
     } else {
       return (
         <>
           <img src={chuteImage}></img>
+          <p className="mx-1">
+            <Link to="/Signup">Sign up to play!</Link>
+          </p>
+          <img src={tttImage}></img>
           <p className="mx-1">
             <Link to="/Signup">Sign up to play!</Link>
           </p>
