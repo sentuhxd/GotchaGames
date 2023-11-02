b78f6c699cfe053747bd89b1712fe814c942d8e6
MaddyKm <134648462+MaddyKm@users.noreply.github.com>
2023-11-02 12:00:15 -0400

    adds board game structure

--- client/src/components/Games/index.jsx
+++ client/src/components/Games/index.jsx
@@ -9,7 +9,7 @@ function Games() {
         <>
           <img src={chuteImage}></img>
           <p className="mx-1">
-            <Link to="/Home">Play Game</Link>
+            <Link to="/Chutes">Play Game</Link>
           </p>
         </>
       );
---
+++ client/src/pages/Chutes.jsx
@@ -0,0 +1,12 @@
+import ChutesAndLadders from "../components/ChutesAndLadders/";
+
+const Chutes = () => {
+  return (
+    <div className="container">
+      <h1>Chutes and Ladders</h1>
+      <ChutesAndLadders />
+    </div>
+  );
+};
+
+export default Chutes;
