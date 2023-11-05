dea6f2138179526f840538451d2f418cb9086026
pineonline10 <joshisstillpine@gmail.com>
2023-11-04 21:25:02 -0400

    will continue later

--- client/src/chickenBingoBoard.jsx
+++ client/src/chickenBingoBoard.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import chickenGif from './assets/Chicken_Feed_Light_Brown.gif';
 
 const GRID_SIZE = 5;
 
@@ -9,53 +10,77 @@ const ChickenBingoBoard = ({ G, moves, ctx }) => {
     console.log("Rendering ChickenBingoBoard", G, ctx);
 
     const handleCellClick = (x, y) => {
-        moves.placeBet(x, y);
+        if (ctx.phase === 'setup') {
+            moves.placeBet(x, y);
+        }
     };
 
     const currentPlayerCard = G.players[ctx.currentPlayer]?.card || [];
+    const currentPlayerBet = G.players[ctx.currentPlayer]?.bet;
+
+    // Function to render game over message
+    const renderGameOverMessage = () => {
+        if (ctx.gameover) {
+            return ctx.gameover.winner ? (
+                <div>The winner is player {ctx.gameover.winner}!</div>
+            ) : (
+                <div>It's a draw!</div>
+            );
+        }
+        return null;
+    };
 
     return (
-        <div style={{ display: 'grid', gridTemplateColumns: `repeat(${GRID_SIZE}, 60px)`, gap: '5px' }}>
-            {currentPlayerCard.map((row, x) => (
-                row.map((cell, y) => (
-                    <div 
-                        key={`${x}-${y}`}
-                        onClick={() => handleCellClick(x, y)}
-                        onMouseEnter={() => setHoveredCell({ x, y })}
-                        onMouseLeave={() => setHoveredCell(null)}
-                        style={{
+        <div>
+            {/* Render game over message if applicable */}
+            {renderGameOverMessage()}
+
+            {/* Display the chicken GIF above the game board */}
+            <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+                <img src={chickenGif} alt="Animated chicken" />
+            </div>
+    
+            {/* Game board grid */}
+            <div style={{ display: 'grid', gridTemplateColumns: `repeat(${GRID_SIZE}, 60px)`, gap: '5px' }}>
+                {currentPlayerCard.map((row, x) => (
+                    row.map((cell, y) => {
+                        const isBetCell = currentPlayerBet && currentPlayerBet.x === x && currentPlayerBet.y === y;
+                        const isPoopCell = G.chickenPosition && G.chickenPosition.x === x && G.chickenPosition.y === y;
+                        const cellStyle = {
                             width: '60px',
                             height: '60px',
                             border: '1px solid black',
-                            backgroundColor: cell ? 'brown' : hoveredCell?.x === x && hoveredCell?.y === y ? 'lightgray' : 'white',
+                            backgroundColor: isPoopCell ? 'brown' :
+                                            isBetCell ? 'blue' :
+                                            hoveredCell?.x === x && hoveredCell?.y === y ? 'lightgray' : 'white',
                             display: 'flex',
                             alignItems: 'center',
                             justifyContent: 'center',
-                            cursor: 'pointer',
+                            cursor: ctx.phase === 'setup' ? 'pointer' : 'default',
                             transition: 'background-color 0.2s'
-                        }}>
-                        {cell ? "💩" : ""}
-                    </div>
-                ))
-            ))}
+                        };
+    
+                        return (
+                            <div 
+                                key={`${x}-${y}`}
+                                onClick={() => handleCellClick(x, y)}
+                                onMouseEnter={() => setHoveredCell({ x, y })}
+                                onMouseLeave={() => setHoveredCell(null)}
+                                style={cellStyle}>
+                                {isPoopCell ? "💩" : ""}
+                            </div>
+                        );
+                    })
+                ))}
+            </div>
         </div>
     );
 };
 
-// ChickenBingoBoard.propTypes = {
-//     G: PropTypes.shape({
-//         players: PropTypes.objectOf(
-//             PropTypes.shape({
-//                 card: PropTypes.arrayOf(
-//                     PropTypes.arrayOf(PropTypes.bool)
-//                 ).isRequired,
-//             }).isRequired
-//         ).isRequired
-//     }).isRequired,
-//     ctx: PropTypes.object.isRequired,
-//     moves: PropTypes.shape({
-//         placeBet: PropTypes.func.isRequired
-//     }).isRequired
-// };
+ChickenBingoBoard.propTypes = {
+    G: PropTypes.object.isRequired,
+    moves: PropTypes.objectOf(PropTypes.func).isRequired,
+    ctx: PropTypes.object.isRequired
+};
 
 export default ChickenBingoBoard;
--- client/src/chickenBingoGame.jsx
+++ client/src/chickenBingoGame.jsx
@@ -1,83 +1,117 @@
+import { INVALID_MOVE } from "boardgame.io/core";
+
 const GRID_SIZE = 5;
 
 const initializeCard = () => {
-    let card = [];
-    for (let i = 0; i < GRID_SIZE; i++) {
-        let row = [];
-        for (let j = 0; j < GRID_SIZE; j++) {
-            row.push(false);
-        }
-        card.push(row);
+  let card = [];
+  for (let i = 0; i < GRID_SIZE; i++) {
+    let row = [];
+    for (let j = 0; j < GRID_SIZE; j++) {
+      row.push(false);
     }
-    return card;
+    card.push(row);
+  }
+  return card;
 };
 
 export const ChickenBingo = {
-    setup: ({ ctx }) => {
-        console.log("Setting up ChickenBingo");
-        let playerCards = {};
-        console.log(ctx.numPlayers);
-        for (let i = 0; i < ctx.numPlayers; i++) {
-            playerCards[i] = {
-                card: initializeCard(),
-                bet: null
-            };
-        }
-        return {
-            players: playerCards,
-            chickenPosition: null,
-        };
-    },
-    moves: {
+  setup: ({ ctx }) => {
+    console.log("Setting up ChickenBingo");
+    let playerCards = {};
+    console.log(ctx.numPlayers);
+    for (let i = 0; i < ctx.numPlayers; i++) {
+      playerCards[i] = {
+        card: initializeCard(),
+        bet: null,
+        ready: false,
+      };
+    }
+    return {
+      players: playerCards,
+      chickenPosition: null,
+    };
+  },
+  phases: {
+    setup: {
+      start: true,
+      next: 'play',
+      onBegin: (G, ctx) => {
+        console.log('Game is in setup phase.');
+      },
+      onEnd: (G, ctx) => {
+        console.log('Setup phase is ending, all players are ready.');
+      },
+      moves: {
         placeBet: (G, ctx, x, y) => {
-            console.log("placeBet called with", x, y);
-            
-            // Log the current state for debugging
-            console.log("Current Player: ", ctx.currentPlayer);
-            console.log("Players: ", G.players);
-    
-            // Check if players object exists and the current player is defined
-            if (G.players && G.players.hasOwnProperty(ctx.currentPlayer)) {
-                G.players[ctx.currentPlayer].bet = { x, y };
-                console.log(`Bet placed for player ${ctx.currentPlayer}:`, G.players[ctx.currentPlayer].bet);
-            } else {
-                // Log an error if the player object is not found
-                console.error('Undefined player object', ctx.currentPlayer, G.players);
-            }
+          // Check if the bet is within bounds
+          if (x < 0 || x >= GRID_SIZE || y < 0 || y >= GRID_SIZE) {
+            return INVALID_MOVE;
+          }
+          // Record the bet for the current player
+          G.players[ctx.currentPlayer].bet = { x, y };
         },
-
-        chickenPoop: (G) => {
-            let x = Math.floor(Math.random() * GRID_SIZE);
-            let y = Math.floor(Math.random() * GRID_SIZE);
-            console.log("chickenPoop called. Chicken position:", x, y);
-            G.chickenPosition = { x, y };
-        }
+        setReady: (G, ctx) => {
+          G.players[ctx.currentPlayer].ready = true;
+        },
+      },
+      endIf: (G, ctx) => {
+        // End the setup phase if all players are ready
+        if (typeof G.players !== 'object' || G.players === null) {
+            console.error('G.players is undefined or null during setup endIf');
+            return false;
+          } return Object.values(G.players).every(p => p.ready);
+        },
     },
-
-    endIf: (G, ctx) => {
-        if (G.chickenPosition) {
-            for (let playerID in G.players) {
-                if (G.players[playerID].bet && 
-                    G.players[playerID].bet.x === G.chickenPosition.x && 
-                    G.players[playerID].bet.y === G.chickenPosition.y) {
-                    return { winner: playerID };
-                }
-            }
-            return { draw: true };
-        }
+    play: {
+      onBegin: (G, ctx) => {
+        console.log('Play phase has begun.');
+      },
+      moves: {
+        chickenPoop: (G, ctx) => {
+          let x = Math.floor(Math.random() * GRID_SIZE);
+          let y = Math.floor(Math.random() * GRID_SIZE);
+          G.chickenPosition = { x, y };
+        },
+      },
+      endIf: (G, ctx) => {
+        // End condition for the play phase or the game
+      },
+      onEnd: (G, ctx) => {
+        // Logic to execute at the end of the play phase
+      },
     },
-
-    turn: {
-        stages: {
-            betting: {
-                moves: { 
-                    placeBet: (G, ctx, x, y) => {
-                        G.players[ctx.currentPlayer].bet = { x, y };
-                    }
-                }, 
-            }
-        }
+  },
+  turn: {
+    stages: {
+      betting: {
+        moves: {
+          placeBet: (G, ctx, x, y) => {
+            G.players[ctx.currentPlayer].bet = { x, y };
+          },
+        },
+      },
     },
+  },
+  endIf: (G, ctx) => {
+    // Check if G.players is defined and is an object before proceeding
+    if (typeof G.players !== 'object' || G.players === null) {
+      console.error('G.players is undefined or null during game endIf');
+      return false;
+    }
+
+    // Check if the chicken has pooped and if any player has won
+    if (G.chickenPosition) {
+      for (let playerId in G.players) {
+        let player = G.players[playerId];
+        if (player.bet &&
+            player.bet.x === G.chickenPosition.x &&
+            player.bet.y === G.chickenPosition.y) {
+          return { winner: playerId };
+        }
+      }
+      return { draw: true };
+    }
+  },
 };
 
 export default ChickenBingo;
